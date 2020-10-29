62c99e85f827e5298560f8be688e5e1b54d347be
Dong-Yi-Xia <dong7777xia@gmail.com>
2020-10-29 17:56:11 -0400

    added github link

--- src/FirstPageComponent/Footer.js
+++ src/FirstPageComponent/Footer.js
@@ -4,7 +4,7 @@ export default () => {
   return (
     <div className="footer">
       <p>Flatiron School: Mod4 Project @Dong Xia</p>
-
+      <a href="https://github.com/Dong-Yi-Xia" target="_blank"> GitHub Link </a>
     </div>
   )
 }
